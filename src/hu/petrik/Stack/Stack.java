e71d00c4f141cf5937d89fd7d1d8d76d2e97543a
Pivarcsi Gergely Máté <gergely.pivarcsi@gmail.com>
2021-11-11 11:10:19 +0100

    pop, top. isEmpty es empty

--- src/hu/petrik/Stack/Stack.java
+++ src/hu/petrik/Stack/Stack.java
@@ -15,4 +15,20 @@ public class Stack<T> {
     public void push(T element) {
         this.elements.add(element);
     }
+
+    public T pop() {
+        return this.elements.remove(this.elements.size()-1);
+    }
+
+    public T top() {
+        return this.elements.get(this.elements.size()-1);
+    }
+
+    public boolean isEmpty() {
+        return this.elements.isEmpty();
+    }
+
+    public void empty() {
+        this.elements.clear();
+    }
 }
